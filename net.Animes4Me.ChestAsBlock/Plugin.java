b83ec83ff71f3f9891e74e8acccaa8c038081d1e
DieFriiks @ totalegal <undeaD_D@live.de>
2015-10-21 16:34:21 +0200

    Update Plugin.java

--- net.Animes4Me.ChestAsBlock/Plugin.java
+++ net.Animes4Me.ChestAsBlock/Plugin.java
@@ -1,26 +1,22 @@
 package net.Animes4Me.ChestAsBlock;
 	import java.util.Arrays;
 	import java.util.List;
-
 	import org.bukkit.ChatColor;
 	import org.bukkit.command.Command;
 	import org.bukkit.command.CommandSender;
 	import org.bukkit.configuration.file.FileConfiguration;
 	import org.bukkit.plugin.java.JavaPlugin;
-
 	
 	public class Plugin extends JavaPlugin {
 		protected FileConfiguration config;
 		protected CmdExecutor cmd_executor;
 		protected EventManager event_manager;
 		
-		
 		public void onEnable(){
 			loadConfig();		
 			cmd_executor = new CmdExecutor(this);
 			event_manager = new EventManager(this);
 		}
-
 		
 		@Override
 	    	public List<String> onTabComplete(CommandSender sender, Command cmd, String commandLabel, String[] args) {
@@ -30,22 +26,18 @@ package net.Animes4Me.ChestAsBlock;
 			return null;
 		}
 		
-		
 		protected void loadConfig(){
 			config = getConfig();
 			config.options().copyDefaults(true);
 			saveConfig();
 		}
 		
-		
 		protected String getMessage(String path) {
 			return ChatColor.translateAlternateColorCodes('&', config.getString("messages.prefix") + config.getString(path));
 		}
 
-		
 		public void disable() {
 			setEnabled(false);
 		}		
-		
 
 	}
